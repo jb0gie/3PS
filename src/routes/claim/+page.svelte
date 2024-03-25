0917e646bbbdd862c628f4b2f91256dcf686830c
j_b0gie <wxzvrd@protonmail.com>
2024-03-25 03:46:32 +0000

    alrighty time to sleep

--- src/routes/claim/+page.svelte
+++ src/routes/claim/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { buttonVariants } from '$lib/components/ui/button';
+	import { Button, buttonVariants } from '$lib/components/ui/button';
 	import * as Dialog from '$lib/components/ui/dialog';
 	const pic1 = 'https://i.imgur.com/F90cAmF.jpeg';
 	const headStyle =
@@ -89,29 +89,43 @@
 									<h1 class="text-2xl">
 										<code>Enter a realm where you're not just listening to music</code>
 									</h1>
-									<h2 class="text-lg"><code>you're part of the music's journey.</code></h2>
+									<h2 class="text-base"><code>you're part of the music's journey.</code></h2>
 								</Dialog.Title>
 								<Dialog.Description>
-									<h4 class="text-lg">
-										https://www.migs718.com/ https://www.instagram.com/mikeloco
-										https://thirdplanet.studio/ To access the unlockable content:
-									</h4>
-									<ul class="text-xl">
-										<li>
-											1. Go to <a href="https://app.darkblock.io">darkblock</a>
-										</li>
-										<li>2. Click `Connect Wallet` make sure your on `Polygon` network</li>
-										<li>3. Select "What DoI Know" NFT</li>
-										<li>
-											4. Click `Authenticate to access` Pop Up and Sign Transaction to view
-											unlockable content.
-										</li>
-									</ul>
+									<div class="">
+										<div>
+											<a href="https://www.migs718.com/">
+												<Button>migs718.com</Button>
+											</a>
+											<a href="https://www.instagram.com/mikeloco">
+												<Button>@mikeloco on Instagram</Button>
+											</a>
+											<a href="https://thirdplanet.studio/">
+												<Button>3rdPlanetStudio</Button>
+											</a>
+										</div>
 
-									<h1 class="text-base">
-										#EnterTheMigtrix #ExclusiveTrack #NFTAccess #LiveMusicExperience #Migs718
-									</h1>
-								</Dialog.Description>
+										<h4 class="text-lg">To access the unlockable content:</h4>
+										<br />
+
+										<ul class="text-lg">
+											<li>1. Go to <a href="https://app.darkblock.io">darkblock</a></li>
+											<br />
+											<li>2. Click `Connect Wallet` make sure your on `Polygon` network</li>
+											<br />
+											<li>3. Select "What DoI Know" NFT</li>
+											<br />
+											<li>
+												4. Click `Authenticate to access` Pop Up and Sign Transaction to view
+												unlockable content.
+											</li>
+										</ul>
+										<br />
+										<h1 class="text-base">
+											#EnterTheMigtrix #ExclusiveTrack #NFTAccess #LiveMusicExperience #Migs718
+										</h1>
+									</div></Dialog.Description
+								>
 							</Dialog.Header>
 						</Dialog.Content>
 					</Dialog.Root>
@@ -125,55 +139,18 @@
 		</div>
 	</div>
 </section>
-
-<!-- <section class="container mx-auto p-10 md:py-20 px-0 md:p-10 md:px-0">
-	<section
-		class="relative px-10 md:p-0 transform duration-500 hover:shadow-2xl cursor-pointer hover:-translate-y-1"
-	>
-		<img class="xl:max-w-6xl rounded-lg" src={pic1} alt="CLAIM NOW" />
-		<div
-			class="content supports-[backdrop-filter]:bg-background/60 bg-background/95 shadow-sm backdrop-blur p-2 pt-8 md:p-12 pb-12 lg:max-w-lg w-full lg:absolute top-48 right-5 rounded-xl"
-		>
-			<h1 class="text-3xl font-semibold mt-4 md:mt-10">Enter the Migtrix!</h1>
-			<p class="my-3 text-justify font-medium leading-relaxed">
-				Dive deep into the exclusive pre-release of "What Do I Know" by Migs718, available only
-				through this unique NFT or at select live performances. Experience this special edition of
-				the song in a way that's reserved for the true fans before anyone else.
-			</p>
-			<ul class="my-3 text-justify">
-				<li>
-					Exclusive Access:This version is accessible ONLY with this NFT and at exclusive live
-					events. Unlock the Music: Be part of a select audience that gets to witness this rare
-					musical fusion. Special Airdrop: The special version will be airdropped to NFT holders,
-					distinct from the version released on Digital Service Providers (DSPs).
-				</li>
-				<li />
-				<li />
-			</ul>
-			<p class="my-3 text-justify font-medium leading-relaxed">
-				Mastered Version Launch: Set for 2025, with an airdrop to all NFT holders upon release.
-				Don't miss your chance to be part of this groundbreaking experience. Secure your NFT now
-				and join us in the Migtrix, where digital exclusivity meets live concert vibes.
-			</p>
-			<p class="my-3 text-justify font-medium leading-relaxed">
-				Enter a realm where you're not just listening to music; you're part of the music's
-				journey. https://www.migs718.com/ https://www.instagram.com/mikeloco
-				https://thirdplanet.studio/ To access the unlockable content: 1. Go to app.DarkBlock.io 2.
-				Click `Connect Wallet`, Select `Polygon` 3. Select "What Do I Know" NFT 4. Click
-				`Authenticate to access` Pop Up and Sign Transaction to view unlockable content. You can
-				view Darkblocks using the Darkblock web-app at app.darkblock.io, or the Darkblock TV App
-				on Google or Amazon:
-				https://play.google.com/store/apps/details?id=io.darkblock.darkblock&hl=en_US&gl=US
-				https://www.amazon.com/Darkblock-NFT-Display/dp/B09B4FLQZ6/Amazon #EnterTheMigtrix
-				#ExclusiveTrack #NFTAccess #LiveMusicExperience #Migs718
-			</p>
-			<button
-				class="mt-2 md:mt-5 p-3 px-5 bg-black text-white font-bold text-sm hover:bg-purple-800"
-				>Read More</button
-			>
-		</div>
-	</section>
-</section> -->
+<div class="h-8" />
+<section class="container mx-auto p-8 md:py-20 px-0 md:p-10 md:px-0">
+	<div class="max-w-3xl flex items-center lg:h-[500px] flex-wrap mx-auto my-12 lg:my-2">
+		<iframe
+			src="https://embed.ipfscdn.io/ipfs/bafybeigdie2yyiazou7grjowoevmuip6akk33nqb55vrpezqdwfssrxyfy/erc1155.html?contract=0x450a0Ca9d299a77a61EA6BBa90A60DcD73AFEbE6&amp;chain=%7B%22name%22%3A%22Polygon+Mainnet%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fpolygon.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22matic%22%2C%22chainId%22%3A137%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22polygon%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Fpolygon%2F512.png%22%2C%22width%22%3A512%2C%22height%22%3A512%2C%22format%22%3A%22png%22%7D%7D&amp;clientId=f0befac3890d5157677c9e54c7713da5&amp;tokenId=4&amp;theme=light&amp;primaryColor=purple"
+			width="1200px"
+			height="600px"
+			style="max-width:100%;"
+			frameborder="0"
+		/>
+	</div>
+</section>
 
 <style>
 	.video-docker {
