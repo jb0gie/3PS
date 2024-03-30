df415f7e01771fba3a60b8c46155a39f44ff24dd
jb0gie <wxzvrd@protonmail.com>
2024-03-30 16:20:34 +0000

    last couple touched

--- src/routes/allaccess/+page.svelte
+++ src/routes/allaccess/+page.svelte
@@ -6,7 +6,7 @@
 <div>
 	<div class="container mx-auto p-4 overflow-hidden md:rounded-lg md:p-10 lg:p-12">
 		<PageTitle>
-			<svelte:fragment slot="pageName">All Access</svelte:fragment>
+			<svelte:fragment slot="pageName">All Access Pass</svelte:fragment>
 		</PageTitle>
 		<AaHero />
 	</div>
