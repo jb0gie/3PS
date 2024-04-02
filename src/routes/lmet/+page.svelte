68d8df91645efb2d491d75f2fc6c2ec8220700a9
jb0gie <wxzvrd@protonmail.com>
2024-04-02 07:45:38 +0100

    lmet?

---
+++ src/routes/lmet/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import PageTitle from '$lib/components/PageTitle/PageTitle.svelte';
+	import LMetHero from './(components)/LMetHero.svelte';
+</script>
+
+<div>
+	<div class="container mx-auto p-4 overflow-hidden md:rounded-lg md:p-10 lg:p-12">
+		<PageTitle>
+			<svelte:fragment slot="pageName">Lmet mint</svelte:fragment>
+		</PageTitle>
+		<LMetHero />
+	</div>
+</div>
