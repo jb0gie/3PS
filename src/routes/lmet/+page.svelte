342f2ccc352aa6f728aa86bd1865c9b9f8326ec4
jb0gie <wxzvrd@protonmail.com>
2024-04-02 08:09:51 +0100

    it's i not l

--- src/routes/lmet/+page.svelte
+++ src/routes/lmet/+page.svelte
@@ -1,13 +1,13 @@
 <script>
 	import PageTitle from '$lib/components/PageTitle/PageTitle.svelte';
-	import LMetHero from './(components)/LMetHero.svelte';
+	import IMetHero from './(components)/IMetHero.svelte';
 </script>
 
 <div>
 	<div class="container mx-auto p-4 overflow-hidden md:rounded-lg md:p-10 lg:p-12">
 		<PageTitle>
-			<svelte:fragment slot="pageName">Lmet mint</svelte:fragment>
+			<svelte:fragment slot="pageName">I Met mint</svelte:fragment>
 		</PageTitle>
-		<LMetHero />
+		<IMetHero />
 	</div>
 </div>
