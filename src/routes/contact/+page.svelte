c712a3ae2f88ead98464adfdf18b155fb5b9f965
jb0gie <wxzvrd@protonmail.com>
2024-11-25 14:50:24 +0000

    added another form to /contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,6 +3,7 @@
 	import PageTitle from '../../lib/components/PageTitle/PageTitle.svelte';
 	import LogoPlanet from './(components)/LogoPlanet.svelte';
 	import third_planet from '$lib/images/logo-s.png';
+	import * as Tabs from '$lib/components/ui/tabs';
 </script>
 
 <div>
@@ -15,17 +16,38 @@
 				<LogoPlanet />
 			</Canvas>
 		</div>
+
 		<div class="space-y-10 text-center flex flex-col items-center">
-			<iframe
-				class="hide-scrollbar rounded-lg h-[520px] overflow-hidden"
-				title="ThirdPlanetEvents"
-				src="https://docs.google.com/forms/d/e/1FAIpQLSe4bJpmLCWR3sQSFDAn5Ky9Hy7TlObfJOM2hApNODFwS5HBZA/viewform?embedded=true"
-				style="border-width:0 scrollbar-color: hsl(215.4 16.3% 46.9% / 0.3); "
-				width="620px"
-				height="100%"
-				frameborder="0"
-				scrolling="yes"
-			/>
+			<Tabs.Root value="inquiry" class="w-auto">
+				<Tabs.List >
+					<Tabs.Trigger value="inquiry">Contact</Tabs.Trigger>
+					<Tabs.Trigger value="art">Art Submission</Tabs.Trigger>
+				</Tabs.List>
+				<Tabs.Content value="inquiry">
+					<iframe
+						class="hide-scrollbar rounded-lg h-[520px] overflow-hidden"
+						title="ThirdPlanetEvents"
+						src="https://docs.google.com/forms/d/e/1FAIpQLSeaSCKel9E5UR3Caf4cuiehgmyCPelFPNK9pB_L0f23y-G63A/viewform?embedded=true"
+						style="border-width:0 scrollbar-color: hsl(215.4 16.3% 46.9% / 0.3); "
+						width="w-auto"
+						height="100%"
+						frameborder="0"
+						scrolling="yes"
+					/>
+				</Tabs.Content>
+				<Tabs.Content value="art">
+					<iframe
+						class="hide-scrollbar rounded-lg h-[520px] overflow-hidden"
+						title="ThirdPlanetEvents"
+						src="https://docs.google.com/forms/d/e/1FAIpQLSe4bJpmLCWR3sQSFDAn5Ky9Hy7TlObfJOM2hApNODFwS5HBZA/viewform?embedded=true"
+						style="border-width:0 scrollbar-color: hsl(215.4 16.3% 46.9% / 0.3); "
+						width="w-auto"
+						height="100%"
+						frameborder="0"
+						scrolling="yes"
+					/>
+				</Tabs.Content>
+			</Tabs.Root>
 		</div>
 	</div>
 </div>
