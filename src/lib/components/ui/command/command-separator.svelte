79f4551b3ca726b62708676fc009ebaf0e5b19d8
web3genie <wxzvrd@protonmail.com>
2025-02-13 18:21:57 +0000

    let's try this agen

--- src/lib/components/ui/command/command-separator.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { Command as CommandPrimitive } from "cmdk-sv";
-	import { cn } from "$lib/utils";
-	let className = undefined;
-	export { className as class };
-</script>
-
-<CommandPrimitive.Separator class={cn("-mx-1 h-px bg-border", className)} {...$$restProps} />
