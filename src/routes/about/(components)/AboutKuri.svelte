d1ba4345625c4291c89d1c53ebbea4c41896e3c6
web3genie <wxzvrd@protonmail.com>
2025-02-18 14:20:46 +0000

    cooking

--- src/routes/about/(components)/AboutKuri.svelte
+++ src/routes/about/(components)/AboutKuri.svelte
@@ -11,7 +11,7 @@
 	const gif0 = 'https://i.giphy.com/media/jtvB77L0afuXGlO5Uh/giphy-downsized-large.gif';
 </script>
 
-<!-- Temporarily commented out until image issue is resolved
+
 <div class="relative overflow-hidden rounded-xl">
 	<div class="mx-auto max-w-7xl">
 		<div class="relative z-10 pb-4">
@@ -47,42 +47,6 @@
 		</div>
 	</div>
 </div>
--->
 
-<!-- Temporary placeholder -->
-<div class="relative overflow-hidden rounded-xl">
-	<div class="mx-auto max-w-7xl">
-		<div class="relative z-10 pb-4">
-			<div class="pt-1"></div>
-			<OrbGlow />
-			<main
-				class="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
-			>
-				<div class="sm:text-center lg:text-left">
-					<h2 class="my-2 text-2xl tracking-tight sm:text-6xl md:text-7xl">
-						<b>KURI</b>
-					</h2>
-					<h2 class="my-2 text-2xl tracking-tight sm:text-6xl md:text-4xl">
-						<b>"<i>THE VTUBER</i>"</b>
-					</h2>
-					<blockquote class="mt-10">
-						<p class="text-xl leading-loose">
-							Visual Artist / Designer / VTuber! Kuri specializes in 3D and VFX. Their styles
-							include "Y2K" or Depthcore. They are deeply influenced by postmodernism, often
-							incorporating its principles into his work.
-							<br />
-							Being proficient in Photoshop, Blender, Cinema4D, After Effects, and more. Having passion
-							for abstract art is evident in his designs, where they plays with form, color, and space
-							to create unique visuals.
-						</p>
-					</blockquote>
-					<div class="space-y-4 p-4">
-						<h6 class="h6">FOUNDER</h6>
-					</div>
-					<hr class="opacity-50" />
-				</div>
-			</main>
-		</div>
-	</div>
-</div>
--->
+
+
