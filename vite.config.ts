94acb70dd8be5738ccbe63f76480a13eb0938324
web3genie <wxzvrd@protonmail.com>
2025-02-18 14:22:20 +0000

    and this too

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['three', '@threlte/core', '@threlte/extras']
+	},
+	optimizeDeps: {
+		exclude: ['@threlte/core', '@threlte/extras']
+	},
+	server: {
+		fs: {
+			strict: false
+		}
+	}
 });
